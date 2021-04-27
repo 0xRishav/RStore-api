c6d12eeff95cd7c0bb7acd741c31516eb817677e
Rishav Bharti <rshvbharti@gmail.com>
2021-04-27 19:15:07 +0530

    Added timestamps

--- models/user.model.js
+++ models/user.model.js
@@ -3,20 +3,25 @@ require("mongoose-type-email");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "name is required"],
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: mongoose.SchemaTypes.Email,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password can't be empty"],
+    },
   },
-  email: {
-    type: mongoose.SchemaTypes.Email,
-    required: [true, "email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password can't be empty"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
