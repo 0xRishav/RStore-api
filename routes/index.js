7e134115a3aa1101f0966c2f1f363ea01034864c
Rishav Bharti <rshvbharti@gmail.com>
2021-04-29 08:55:23 +0530

    Added product routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/User.controller");
+const productController = require("../controllers/Product.controller");
 
+// User routes
 router.post("/users/signup", userController.signUpUser);
 router.post("/users/signin", userController.signInUser);
+
+// Product routes
+router.get("/products", productController.getAllProducts);
+router.get("/products/:productId", productController.getProductById);
 
 module.exports = router;
