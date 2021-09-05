60019ad2bde74dba349de90dda91648f7c593b59
Rishav Bharti <64918176+iamrishavb@users.noreply.github.com>
2021-09-05 21:02:27 +0530

    Integrated payments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
+const shortid = require("shortid");
 const router = express.Router();
+const Razorpay = require("razorpay");
+const User = require("../models/user.model");
+const Cart = require("../models/cart.model");
 
 app.use(cors());
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY,
+  key_secret: process.env.RAZORPAY_KEY,
+});
 
 const userController = require("../controllers/User.controller");
 const productController = require("../controllers/Product.controller");
@@ -31,5 +40,56 @@ router.delete(
   wishlistConroller.removeFromWishlist
 );
 router.get("/wishlist/:id", wishlistConroller.getAllwishlistItems);
+
+// Order Route
+
+async function getCartTotal(userId) {
+  const user = await User.findById(userId);
+  const cart = await Cart.findById(user.cart);
+  if (cart) {
+    const data = await cart.execPopulate({
+      path: "cartItems",
+      populate: { path: "product" },
+    });
+
+    const getTotalPriceReducer = (acc, product) => {
+      return acc + product.product.price * product.quantity;
+    };
+
+    const cartTotal = data.cartItems.reduce(getTotalPriceReducer, 0);
+    return cartTotal;
+  }
+}
+
+router.post("/checkout", async (req, res) => {
+  const userId = req.body.userId;
+  const checkoutPrice = await getCartTotal(userId);
+
+  const payment_capture = 1;
+  const amount = checkoutPrice;
+  const currency = "INR";
+
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
+
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "failed to create payment please try again later" });
+  }
+});
 
 module.exports = router;
