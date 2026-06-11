903b835a5b7a86bf95db6e0ada839f03a23387e4
0xRishav <rshvbharti@gmail.com>
2026-06-11 17:06:15 +0530

    add JWT authentication and strip password from responses
    - Signup/signin return JWT token via authService
    - Strip password from all API responses
    - Return 409 for duplicate email (was 208)

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -1,86 +1,22 @@
-const userDb = require("../models/user.model");
+const asyncHandler = require("../utils/asyncHandler");
+const authService = require("../services/authService");
 
-module.exports.signUpUser = async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = null;
-  try {
-    if (await userDb.findOne({ email: email })) {
-      return res.status(208).json({
-        success: false,
-        message: "This email is already registered",
-      });
-    }
-    if (
-      name &&
-      email &&
-      new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$").test(email) &&
-      password
-    ) {
-      user = {
-        email: email,
-        name: name,
-        password: password,
-      };
-      userDb.create(user);
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "please enter valid email",
-      });
-    }
-    if (user) {
-      return res
-        .status(200)
-        .json({ success: true, message: "User added successfully", user });
-    }
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(503)
-      .json({ success: false, message: "internal server error" });
-  }
-};
+exports.signUpUser = asyncHandler(async (req, res) => {
+  const { token, user } = await authService.signup(req.body);
+  return res.status(201).json({
+    success: true,
+    message: "User added successfully",
+    token,
+    user,
+  });
+});
 
-module.exports.signInUser = async (req, res) => {
-  const { email, password } = req.body;
-  let user = null;
-  try {
-    if (
-      email &&
-      new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$").test(email) &&
-      password
-    ) {
-      user = await userDb.findOne({ email: email });
-      if (user) {
-        if (user.password === password) {
-          return res.status(200).json({
-            success: true,
-            message: "Sign In successfull",
-            user
-          });
-        } else {
-          return res.status(401).json({
-            success: false,
-            message: "Incorrect Password",
-          });
-        }
-      } else {
-        return res.status(400).json({
-          success: false,
-          message: "User not found",
-        });
-      }
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "email or password invalid",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "internal server error",
-    });
-  }
-};
+exports.signInUser = asyncHandler(async (req, res) => {
+  const { token, user } = await authService.signin(req.body);
+  return res.status(200).json({
+    success: true,
+    message: "Sign In successful",
+    token,
+    user,
+  });
+});
