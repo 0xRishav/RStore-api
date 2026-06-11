d9b77b484a76ddc234c076e55fc1ef35dc17f6a0
0xRishav <rshvbharti@gmail.com>
2026-06-11 17:07:21 +0530

    refactor: rewrite controllers to use service layer and asyncHandler

--- controllers/Cart.controller.js
+++ controllers/Cart.controller.js
@@ -1,148 +1,45 @@
-const cartdb = require("../models/cart.model");
-const userdb = require("../models/user.model");
+const asyncHandler = require("../utils/asyncHandler");
+const cartService = require("../services/cartService");
 
-module.exports.addToCart = async (req, res) => {
-  const { id } = req.params;
-  const { productId } = req.body;
-  try {
-    const user = await userdb.findById(id);
-    let cart = await cartdb.findById(user.cart);
-    if (cart) {
-      if (!cart.cartItems.some(({ product }) => product == productId)) {
-        cart.cartItems.push({
-          product: productId,
-          quantity: 1,
-        });
-        await cart.save();
-      }
-    } else {
-      cart = await cartdb.create({
-        by: id,
-      });
-      user.cart = cart._id;
-      await user.save();
-      cart.cartItems.push({
-        product: productId,
-        quantity: 1,
-      });
-      await cart.save();
-    }
-    const populatedCart = await cart.populate({
-      path: "cartItems",
-      populate: { path: "product" },
-    });
-    return res.status(201).json({
-      success: true,
-      data: [...populatedCart.cartItems],
-      message: "Product added to cart",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "Internal server error",
-    });
-  }
-};
+exports.addToCart = asyncHandler(async (req, res) => {
+  const data = await cartService.addToCart(req.params.id, req.body.productId);
+  return res.status(201).json({
+    success: true,
+    data,
+    message: "Product added to cart",
+  });
+});
 
-module.exports.removeFromCart = async (req, res) => {
-  const { id, productId } = req.params;
-  try {
-    const user = await userdb.findById(id);
-    const cart = await cartdb.findById(user.cart);
-    if (cart.cartItems.some(({ product }) => product == productId)) {
-      await cart.updateOne({ $pull: { cartItems: { product: productId } } });
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid request",
-      });
-    }
-    const populatedCart = await cartdb.findById(user.cart).populate({
-      path: "cartItems",
-      populate: { path: "product" },
-    });
-    return res.status(201).json({
-      success: true,
-      data: [...populatedCart.cartItems],
-      message: "Product removed from cart",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "internal server error",
-    });
-  }
-};
+exports.removeFromCart = asyncHandler(async (req, res) => {
+  const data = await cartService.removeFromCart(
+    req.params.id,
+    req.params.productId
+  );
+  return res.status(200).json({
+    success: true,
+    data,
+    message: "Product removed from cart",
+  });
+});
 
-module.exports.changeQuantity = async (req, res) => {
-  const { id, productId } = req.params;
-  const { quantity } = req.body;
-  try {
-    const user = await userdb.findById(id);
-    const cart = await cartdb.findById(user.cart);
-    if (
-      cart.cartItems.some(({ product }) => product == productId) &&
-      quantity > 0
-    ) {
-      await cart.updateOne({ $pull: { cartItems: { product: productId } } });
-      cart.cartItems.push({
-        product: productId,
-        quantity: quantity,
-      });
-      await cart.save();
-      const populatedCart = await cartdb.findById(user.cart).populate({
-        path: "cartItems",
-        populate: { path: "product" },
-      });
-      return res.status(201).json({
-        success: true,
-        data: [...populatedCart.cartItems],
-        message: "Product quantity updated",
-      });
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid request",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "internal server error",
-    });
-  }
-};
+exports.changeQuantity = asyncHandler(async (req, res) => {
+  const data = await cartService.changeQuantity(
+    req.params.id,
+    req.params.productId,
+    req.body.quantity
+  );
+  return res.status(200).json({
+    success: true,
+    data,
+    message: "Product quantity updated",
+  });
+});
 
-module.exports.getAllCartItems = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await userdb.findById(id);
-    const cart = await cartdb.findById(user.cart);
-    if (cart) {
-      const data = await cart.populate({
-        path: "cartItems",
-        populate: { path: "product" },
-      });
-      return res.status(200).json({
-        success: true,
-        data: [...data.cartItems],
-        message: "Have some cartitems",
-      });
-    } else {
-      return res.status(200).json({
-        success: true,
-        data: [],
-        message: "Cart hasn't been created yet",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "internal server error",
-    });
-  }
-};
+exports.getAllCartItems = asyncHandler(async (req, res) => {
+  const data = await cartService.getAllCartItems(req.params.id);
+  return res.status(200).json({
+    success: true,
+    data,
+    message: data.length ? "Cart items retrieved" : "Cart is empty",
+  });
+});
--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -1,108 +1,35 @@
-const userdb = require("../models/user.model");
-const wishlistdb = require("../models/wishlist.model");
+const asyncHandler = require("../utils/asyncHandler");
+const wishlistService = require("../services/wishlistService");
 
-module.exports.addToWishlist = async (req, res) => {
-  const { id } = req.params;
-  const { productId } = req.body;
-  try {
-    const user = await userdb.findById(id);
-    let wishlist = await wishlistdb.findById(user.wishlist);
-    if (wishlist) {
-      if (!wishlist.wishlistItems.some((product) => product == productId)) {
-        wishlist.wishlistItems.push(productId);
-        await wishlist.save();
-      }
-    } else {
-      wishlist = await wishlistdb.create({
-        by: id,
-      });
-      user.wishlist = wishlist._id;
-      await user.save();
-      wishlist.wishlistItems.push(productId);
-      await wishlist.save();
-    }
-    const populatedWishlist = await wishlist.populate({
-      path: "wishlistItems",
-      populate: { path: "products" },
-    });
-    return res.status(201).json({
-      success: true,
-      data: [...populatedWishlist.wishlistItems],
-      message: "Product added to wishlist",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "Internal error",
-    });
-  }
-};
+exports.addToWishlist = asyncHandler(async (req, res) => {
+  const data = await wishlistService.addToWishlist(
+    req.params.id,
+    req.body.productId
+  );
+  return res.status(201).json({
+    success: true,
+    data,
+    message: "Product added to wishlist",
+  });
+});
 
-module.exports.removeFromWishlist = async (req, res) => {
-  const { id, productId } = req.params;
-  try {
-    const user = await userdb.findById(id);
-    const wishlist = await wishlistdb.findById(user.wishlist);
-    if (wishlist.wishlistItems.some((product) => product == productId)) {
-      await wishlist.updateOne({ $pull: { wishlistItems: productId } });
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid request",
-      });
-    }
-    const populatedWishlist = await wishlistdb.findById(user.wishlist).populate({
-      path: "wishlistItems",
-      populate: { path: "products" },
-    });
-    return res.status(201).json({
-      success: true,
-      data: [...populatedWishlist.wishlistItems],
-      message: "Product removed from wishlist",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      success: false,
-      message: "Internal error",
-    });
-  }
-};
+exports.removeFromWishlist = asyncHandler(async (req, res) => {
+  const data = await wishlistService.removeFromWishlist(
+    req.params.id,
+    req.params.productId
+  );
+  return res.status(200).json({
+    success: true,
+    data,
+    message: "Product removed from wishlist",
+  });
+});
 
-module.exports.getAllwishlistItems = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await userdb.findById(id);
-    if (!user) {
-      return res.status(404).json({
-        ok: false,
-        message: "User not found",
-      });
-    }
-    const wishlist = await wishlistdb.findById(user.wishlist);
-    if (wishlist) {
-      const data = await wishlist.populate({
-        path: "wishlistItems",
-        populate: { path: "products" },
-      });
-      return res.status(201).json({
-        ok: true,
-        data: [...data.wishlistItems],
-        message: "Product added to wishlist",
-      });
-    } else {
-      return res.status(200).json({
-        ok: true,
-        data: [],
-        message: "Wishlist hasn't been created yet",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      ok: false,
-      message: "Internal error",
-    });
-  }
-};
+exports.getAllwishlistItems = asyncHandler(async (req, res) => {
+  const data = await wishlistService.getAllWishlistItems(req.params.id);
+  return res.status(200).json({
+    success: true,
+    data,
+    message: data.length ? "Wishlist items retrieved" : "Wishlist is empty",
+  });
+});
