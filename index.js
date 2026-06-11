a1c4bddab6d761b85779641c04b9988f48743374
0xRishav <rshvbharti@gmail.com>
2026-06-11 17:06:25 +0530

    add helmet security headers and lock down CORS to env origin

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const express = require("express");
-var cors = require("cors");
-const router = require("./routes");
+require("dotenv").config();
+const { validateEnv } = require("./config/env");
+validateEnv();
 
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const router = require("./routes");
 const db = require("./config/mongoose");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
-
 const port = process.env.PORT || 9000;
 
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-app.use(cors());
+
 app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("RStore API");
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`express server is running at http://localhost:${port}`);
