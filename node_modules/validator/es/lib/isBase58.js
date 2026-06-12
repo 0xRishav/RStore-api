538293ceafb23d5495c87052619138312daee678
0xRishav <rshvbharti@gmail.com>
2026-06-12 19:53:53 +0530

    chore: untrack node_modules from git

--- node_modules/validator/es/lib/isBase58.js
+++
@@ -1,8 +0,0 @@
-import assertString from './util/assertString';
-
-// Accepted chars - 123456789ABCDEFGH JKLMN PQRSTUVWXYZabcdefghijk mnopqrstuvwxyz
-var base58Reg = /^[A-HJ-NP-Za-km-z1-9]*$/;
-export default function isBase58(str) {
-  assertString(str);
-  return base58Reg.test(str);
-}
--- node_modules/validator/es/lib/isSlug.js
+++
@@ -1,6 +0,0 @@
-import assertString from './util/assertString';
-var charsetRegex = /^[a-z0-9](?!.*[-_]{2,})(?:[a-z0-9_-]*[a-z0-9])?$/;
-export default function isSlug(str) {
-  assertString(str);
-  return charsetRegex.test(str);
-}
